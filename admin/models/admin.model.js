80db1c4198f539ca266ca66c22bb8d094a317f66
Shivam Singh <155008516+Synoize@users.noreply.github.com>
2025-07-13 10:50:15 +0530

    Update admin.model.js

--- admin/models/admin.model.js
+++ admin/models/admin.model.js
@@ -1,22 +1,37 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const adminSchema = new mongoose.Schema({
-    uid: {
-        type : Number,
-        required : true,
-        unique : true,
-    },
-    name: {
-        type : String,
-        required : true,
-        unique : true,
-    },
-    password: {
-        type : String,
-        required : true,
-        unique : true,
-    }
-})
+  uid: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
 
-const Admin = mongoose.model('Admin', adminSchema)
-export default Admin
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+adminSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const Admin = mongoose.model("Admin", adminSchema);
+export default Admin;
